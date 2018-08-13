e6ca180896d39672f19e2ac365f253310d9fbaa8
Carlos Estevão <39769751+carlosestevaobs@users.noreply.github.com>
2018-08-13 09:45:33 -0300

    Formulário
    Exemplo de formulário

---
+++ Introdução/Exemplo7_pt1_index.php
@@ -0,0 +1,16 @@
+<html>
+<head></head>
+<body>
+<form method="POST" action="Exemplo7_pt2_verificar.php">
+Login: <input type="text" name="login" /> 
+<br/> <br/>
+Senha: <input type="password" name="senha"/> <br/> <br/>
+Perfil: <select name="tipo" >
+<option> Selecione </option>
+<option> Aluno </option>
+<option> Professor </option>
+</select> <br/><br/>
+<input type="submit" value="Entrar"/>
+<input type="reset" value="Limpar"/>
+</body>
+</html>
