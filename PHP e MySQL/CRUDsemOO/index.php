62c03c88005ddbceef40da8634dc2da0942eb797
Carlos Estevão <39769751+carlosestevaobs@users.noreply.github.com>
2018-09-25 16:14:04 -0300

    atualização

--- PHP e MySQL/CRUDsemOO/index.php
+++ PHP e MySQL/CRUDsemOO/index.php
@@ -5,11 +5,11 @@ if (isset($_GET['id'])) {
     $botao = "Editar";
     $sql = "SELECT * FROM tb_professor where pro_id = ".$_GET['id'];
         $pesquisar = mysqli_query($conexao, $sql);
-          while ($linha = $pesquisar->fetch_assoc()) {
-           $id = $linha['pro_id'];
-            $nome = $linha['pro_nome'];
-            $senha = $linha['pro_senha'];
-          }
+          $vetor = $pesquisar->fetch_assoc(); 
+           $id = $vetor['pro_id'];
+            $nome = $vetor['pro_nome'];
+            $senha = $vetor['pro_senha'];
+          
 } else {
     $endereco = "salvar.php";
     $botao = "Salvar";
@@ -19,8 +19,12 @@ if (isset($_GET['id'])) {
 }
 ?>
 <html>
-    <head></head>
+    <head>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
     <body>        
+    <div id="cadastro">
+    <h1> Cadastro de Usuarios</h1>
         <form method="POST" action="<?php echo $endereco;?>">
             <input type="hidden" name="id" value="<?php echo $id;?>">
             <input type="text" name="usuario" placeholder="usuário" value="<?php echo $nome;?>"> <br/> <br/>
@@ -28,10 +32,37 @@ if (isset($_GET['id'])) {
             <input type="submit" value="<?php echo $botao;?>">        
             <input type="reset" value="Limpar">        
         </form>
-        <?php        
-        $sql = "SELECT * FROM tb_professor order by pro_id asc";
+    </div>
+    <div id="pesquisa">        
+        <h1> Dados </h1>        
+        <form method="GET" action="index.php">
+            <input type="text" name="pesquisa" placeholder="Digite aqui"> <br>
+            <input type="radio" name="tipo" value="0"> Código <br>
+            <input type="radio" name="tipo" value="1" checked> Usuário <br>
+            <input type="submit" value="Pesquisar">
+        </form>
+        <?php  
+        if (isset($_GET['pesquisa'])) {
+            if ($_GET['tipo'] == 1) {
+                $sql = "SELECT * FROM tb_professor where pro_nome like '%".$_GET['pesquisa']."%' order by pro_id asc;";
+            } else {                
+                $numerica = is_numeric($_GET['pesquisa']);
+
+                if ($numerica == 1) {
+                $sql = "SELECT * FROM tb_professor where pro_id = ".$_GET['pesquisa'].";";
+                }
+                else {
+                    echo "O valor digitado não é um número. Retornamos todos os registros";
+                    $sql = "SELECT * FROM tb_professor order by pro_id asc;";
+                }
+            }
+        } else {
+            $sql = "SELECT * FROM tb_professor order by pro_id asc;";
+        }   
         $pesquisar = mysqli_query($conexao, $sql);
-        echo "<h1> Dados </h1> <table >"
+        $quantidade = mysqli_num_rows($pesquisar);
+       if ($quantidade > 0) {
+        echo " <table >"
         . "<tr><td bgcolor='GreenYellow'> <b> Usuário </b> </td>"
         . "<td bgcolor='GreenYellow'> <b> Senha </b> </td>"
         . "<td bgcolor='GreenYellow'> <b> Editar </b> </td>"
@@ -44,6 +75,9 @@ if (isset($_GET['id'])) {
                     ."<td><a href='excluir.php?id=".$linha['pro_id']."'><img src='imagens/excluir.png' ></a></td>";
         }
         echo "</table>";
-        ?>
+    } else {
+        echo "Nenhum dado encontrado";
+    } ?>
+    </div>
     </body>
 </html>
